b00a0df82ad9839e65753afbb1d8247eeb5b43f7
vivek-kumar-gupta <wisdom655@gmail.com>
2023-04-26 03:43:11 +0530

    Update 0084-largest-rectangle-in-histogram.java

--- java/0084-largest-rectangle-in-histogram.java
+++ java/0084-largest-rectangle-in-histogram.java
@@ -1,6 +1,58 @@
 //Please check this video which has best explaination which is understandable.
 //https://youtu.be/lJLcqDsmYfg
 
+class Solution2 {
+    public int largestRectangleArea(int[] heights) {
+
+        int[] nextSmallerIndices = findNextSmallerIndex(heights);
+        int[] prevSmallerIndices = findPrevSmallerIndex(heights);
+        int maxArea = 0;
+        for (int i = 0; i < heights.length; i++){
+
+            maxArea = Math.max(maxArea, ((nextSmallerIndices[i] == - 1 
+            ? heights.length : nextSmallerIndices[i]) - prevSmallerIndices[i] - 1) * heights[i]);
+        }
+        return maxArea;
+    }
+
+
+    private int[] findNextSmallerIndex(int[] heights){
+        Stack<Integer> stack = new Stack<>();
+        int[] result = new int[heights.length];
+        stack.push(-1);
+
+        for (int i = heights.length - 1; i >=0 ; i--){
+           while (stack.peek() != -1 && heights[stack.peek()] >= heights[i]){
+                  stack.pop();
+            }
+
+            result[i] = stack.peek();
+            stack.push(i);
+            
+
+        }
+        return result;
+
+    }
+
+    private int[] findPrevSmallerIndex(int[] heights){
+        Stack<Integer> stack = new Stack<>();
+        int[] result = new int[heights.length];
+        stack.push(-1);
+
+        for (int i = 0; i < heights.length ; i++){
+            while (stack.peek() != -1 && heights[stack.peek()] >= heights[i]){
+                  stack.pop();
+            }
+            result[i] = stack.peek();
+            stack.push(i);
+            
+
+        }
+        return result;
+    }
+}
+
 class Solution {
     public int largestRectangleArea(int[] heights) {
       int area = 0, n = heights.length;
